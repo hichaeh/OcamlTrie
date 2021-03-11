264ebd30609418336a57a47598552e49d0b071d7
hichaeh <hichem.rami.aitelhara@gmail.com>
2021-03-11 03:22:35 +0100

    changed the scructure, made it into a functor

--- src/trie.mli
+++ src/trie.mli
@@ -1,16 +1,20 @@
 
 
-(** A Trie is represented as a list of trees *)
-type 'a t 
+module type OrderedType =
+  sig
+    type t
+    val compare: t -> t -> int
+  end
 
-val empty_trie : unit -> 'a t
+module type T = 
+  sig
+    type v 
+    type t 
+    val empty_trie : unit -> t
+    val add_vl : t -> v list -> t
+    val mem_vl : t -> v list -> bool
+    val rm_vl : t -> v list -> t
+    val get_combs : v list -> t -> (v list) list 
+  end
 
-val add_vl : 'a t -> 'a list -> 'a t
-
-val mem_vl : 'a t -> 'a list -> bool
-
-val rm_vl : 'a t -> 'a list -> 'a t
-
-(** Given a list of elements (the pool) and a trie 
-  * Returns a list of every possible combination from the trie *)
-val get_combs : 'a list -> 'a t -> ('a list) list 
+module Make (V : OrderedType) : T with type v = V.t
